005c90855dcbed7ef28c4482add5ac0091ad255d
Jack Grahl <jack.grahl@gmail.com>
2025-04-27 23:24:47 +0100

    Not used.

--- src/main/java/com/example/examplemod/item/ModItems.java
+++ src/main/java/com/example/examplemod/item/ModItems.java
@@ -5,7 +5,6 @@ import net.minecraft.world.food.FoodProperties;
 import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.level.block.Block;
-import net.minecraft.world.level.block.SoundType;
 import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
